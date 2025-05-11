8c37f40424f992c0681469e162890104ccd5067a
alisonrgalindo <alissonricardogalindo@gmail.com>
2025-05-11 13:05:47 -0300

    feat: setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ChartDashboard } from "./components/ChartDashboard"
+
+function App() {
+
+  return (
+    <div className="p-4">
+      <ChartDashboard />
+    </div>
+  )
+}
+
+export default App
